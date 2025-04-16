1d8ec1b7bf3550f567e071fdd3bc5dcd41e434ce
AbdulRasheed Oduola <rasheedoduola007@gmail.com>
2025-04-16 08:21:29 +0100

    initialize React Todo App with Vite setup and basic components

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+
+function App() {
+  const [todos, setTodos] = useState([])
+  const [todoValue, setTodoValue] = useState('')
+
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({ todos: newList }))
+  }
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
+
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index
+    })
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo(index) {
+    const valueToBeEdited = todos[index]
+    setTodoValue(valueToBeEdited)
+    handleDeleteTodo(index)
+  }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return
+    }
+
+    let localTodos = localStorage.getItem('todos')
+    if (!localTodos) {
+      return
+    }
+
+    console.log(localTodos)
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+
+  }, [])
+
+
+  return (
+    <>
+      <TodoInput
+        handleAddTodos={handleAddTodos}
+        todoValue={todoValue} // Pass todoValue
+        setTodoValue={setTodoValue} // Pass setTodoValue
+      />
+      <TodoList handleEditTodos={handleEditTodo} handleDeleteTodos={handleDeleteTodo} todos={todos} />
+    </>
+  );
+}
+
+export default App;
